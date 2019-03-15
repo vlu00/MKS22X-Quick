fa91c7422352d46af4f58b1502488abd5e8f0466
vlu00 <vlu00@stuy.edu>
2019-03-15 01:35:49 -0400

    got rid of extra methods and added comments

--- Quick.java
+++ Quick.java
@@ -4,42 +4,29 @@ import java.util.*;
 
 public class Quick{
 
-  public static String toString(int [] data) {
-    String display = "";
-    for (int i = 0; i < data.length; i++) {
-      display += data[i] + " ";
-    }
-    return display;
-  }
-
   public static int quickselect(int[] data, int k) {
-    int max = data.length-1;
-    int min = 0;
+    int max = data.length-1; //upper bound. Ignore numbers after this
+    int min = 0; //lower bound. Ignore number before this
     int pivot = partition(data, 0, data.length-1);
 
-    while (pivot != k) {
+    while (pivot != k) { //if kth element not placed
       if (k < pivot) {
-        max = pivot-1;
-        pivot = partition(data, min, max);
+        max = pivot-1; //change upper bound
+        pivot = partition(data, min, max); //new pivot
       }
       else {
-        min = pivot+1;
-        pivot = partition(data, min, max);
+        min = pivot+1; //change lower bound
+        pivot = partition(data, min, max); //new pivot
       }
     }
     return data[k];
   }
 
-  public static int findPivot(int [] data, int start, int end) {
-/*
-    int first = data[start];
-    int second = data[(start+end)/2];
-    int third = data[end];
-
-    int total = data[start] + data[(start+end)/2] + data[end];
+  private static int findPivot(int [] data, int start, int end) {
+    int total = data[start] + data[(start+end)/2] + data[end]; //total of 3 numbers (first, last and middle element)
     int max = Math.max(data[start], Math.max(data[(start+end)/2], data[end]));
     int min = Math.min(data[start], Math.min(data[(start+end)/2], data[end]));
-    int median = total - max - min;
+    int median = total - max - min; //total subtract max and min is the number in the middle
 
     if (median == data[start]) {
       return start;
@@ -48,135 +35,71 @@ public class Quick{
       return end;
     }
     return (start+end)/2;
-    */
-
-    if (data[start] >= data[(start+end)/2]) {
-      if (data[(start+end)/2] >= data[end]) {
-        return (start+end)/2;
-      }
-      if (data[end] >= data[start]) {
-        return start;
-      }
-      return end;
-    }
-
-    if (data[end] >= data[(start+end)/2]) {
-      return (start+end)/2;
-    }
-    if (data[start] >= data[end]) {
-      return start;
-    }
-    return end;
-
   }
 
   public static int partition (int [] data, int start, int end) {
     if (data.length == 1) {
-      return 0;
+      return 0; //don't have to do everything below
     }
-    
+
     Random rng = new Random();
-    int pivot = rng.nextInt(end-start+1) + start; //findPivot(data, start, end); //
+    int pivot = findPivot(data, start, end); //optimized pivot
     int origStart = start;
 
     int num = data[pivot];
-    data[pivot] = data[start];
-    data[start] = num;
+    data[pivot] = data[start]; //move num to front
+    data[start] = num; //swap whatever was in first position
     start++;
+    boolean moveLeft = true; //to optimize when there are duplicates
 
-    while (start < end) {
+    //sorts
+    while (start < end) { //while there are still unsorted
       int temp = data[start];
-      if (data[start] == num) {
-        if (rng.nextInt(2) == 0) {
-          start++;
+      if (data[start] == num) { //if the numbers are equal
+        if (moveLeft) { //half the time
+          start++; //keep in current position
+          moveLeft = false;
         }
-        else {
-          data[start] = data[end];
+        else { //other half
+          data[start] = data[end]; //move to end
           data[end] = temp;
           end--;
+          moveLeft = true;
         }
       }
-      else if (data[start] > num) {
+      else if (data[start] > num) { //move to end
         data[start] = data[end];
         data[end] = temp;
         end--;
       }
-      else {
+      else { //keep in current position and continue
         start++;
       }
     }
 
-    if (data[start] > num) {
-      data[origStart] = data[start-1];
+    //determines where pivot goes
+    if (data[start] > num) { //if final number checked is greater than num
+      data[origStart] = data[start-1]; //place num before that final number
       data[start-1] = num;
       return start-1;
     }
-    else {
-      data[origStart] = data[start];
+    else { //final number is smaller than num
+      data[origStart] = data[start]; //move smaller number to start
       data[start] = num;
       return start;
     }
   }
 
   public static void quicksort(int[] data) {
-    quicksort(data, 0, data.length-1);
+    quicksort(data, 0, data.length-1); //helper method
   }
 
   public static void quicksort(int[] data, int lo, int hi) {
-    if (lo >= hi) {
-      return;
+    if (lo >= hi) { //if start has reached end
+      return; //done
     }
-    int pivot = partition(data, lo, hi);
-    quicksort(data, lo, pivot-1);
-    quicksort(data, pivot+1, hi);
-  }
-
-
-  public static void main (String[] args) {
-    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
-  int[]MAX_LIST = {1000000000,500,10};
-  for(int MAX : MAX_LIST){
-    for(int size = 31250; size < 2000001; size*=2){
-      long qtime=0;
-      long btime=0;
-      //average of 5 sorts.
-      for(int trial = 0 ; trial <=5; trial++){
-        int []data1 = new int[size];
-        int []data2 = new int[size];
-        for(int i = 0; i < data1.length; i++){
-          data1[i] = (int)(Math.random()*MAX);
-          data2[i] = data1[i];
-        }
-        long t1,t2;
-        t1 = System.currentTimeMillis();
-        Quick.quicksort(data2);
-        t2 = System.currentTimeMillis();
-        qtime += t2 - t1;
-        t1 = System.currentTimeMillis();
-        Arrays.sort(data1);
-        t2 = System.currentTimeMillis();
-        btime+= t2 - t1;
-        if(!Arrays.equals(data1,data2)){
-          System.out.println("FAIL TO SORT!");
-          System.exit(0);
-        }
-      }
-      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
-    }
-    System.out.println();
-  }
-/*
-    int [] A = new int [10000000];
-    for (int i = 0; i < 10000000; i++) {
-      A[i] = 10000000-i;
-    }
-
-    int[] B = new int[] {0, 7, 6, 2, 3, 8, 9};
-    quicksort(B);
-    System.out.println(toString(B));
-    System.out.println(quickselect(A, 4));
-    System.out.println(quickselect(A, 6));
-    System.out.println(quickselect(A, 15));
-    */
+    int pivot = partition(data, lo, hi); //new pivot and one element is in correct position
+    quicksort(data, lo, pivot-1); //sort first half
+    quicksort(data, pivot+1, hi); //sort second half
   }
 }
