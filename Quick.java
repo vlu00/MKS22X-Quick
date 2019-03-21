17f5a5bbde7b15b4f7269c9f3a9a99fea777037a
vlu00 <vlu00@stuy.edu>
2019-03-20 23:59:54 -0400

    Got rid of extra methods and tests. Changed comments

--- Quick.java
+++ Quick.java
@@ -112,8 +112,8 @@ public class Quick{
 
   //trying with insertionsort
   public static void quicksort(int[] data, int lo, int hi) {
-    if (hi-lo+1 < 20) { //if start has reached end
-      insertionsort(data, lo, hi); //done
+    if (hi-lo+1 < 20) { //if length of portion being sorted is less than 20
+      insertionsort(data, lo, hi); //insertionsort yeilds faster result
     }
     else {
       int pivot = partition(data, lo, hi); //new pivot and one element is in correct position
@@ -122,102 +122,4 @@ public class Quick{
     }
   }
 
-  public static void quickSort(int[] data) {
-    quickSort(data, 0, data.length-1); //helper method
-  }
-
-  //trying with insertionsort
-  public static void quickSort(int[] data, int lo, int hi) {
-    if (hi-lo+1 < 25) { //if start has reached end
-      insertionsort(data, lo, hi); //done
-    }
-    else {
-      int pivot = partition(data, lo, hi); //new pivot and one element is in correct position
-      quicksort(data, lo, pivot-1); //sort first half
-      quicksort(data, pivot+1, hi); //sort second half
-    }
-  }
-/*
-  public static void quicksort(int[] data, int lo, int hi) {
-    if (lo >= hi) { //if start has reached end
-      return; //done
-    }
-    int pivot = partition(data, lo, hi); //new pivot and one element is in correct position
-    quicksort(data, lo, pivot-1); //sort first half
-    quicksort(data, pivot+1, hi); //sort second half
-  }
-
-  public static void main(String[] args) {
-    int[] A = new int[] {2, 5, 3, 7, 4, 1, 6};
-    quicksort(A);
-    System.out.println(Arrays.toString(A));
-  }
-  */
-  public static void main(String[]args){
-    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
-    int[]MAX_LIST = {1000000000,10};
-    for(int MAX : MAX_LIST){
-      for(int size = 31250; size < 2000001; size*=4){
-        long qtime=0;
-        long btime=0;
-        //average of 5 sorts.
-        for(int trial = 0 ; trial <=5; trial++){
-          int []data1 = new int[size];
-          int []data2 = new int[size];
-          for(int i = 0; i < data1.length; i++){
-            data1[i] = (int)(Math.random()*MAX);
-            data2[i] = data1[i];
-          }
-          long t1,t2;
-          t1 = System.currentTimeMillis();
-          quicksort(data2);
-          t2 = System.currentTimeMillis();
-          qtime += t2 - t1;
-          t1 = System.currentTimeMillis();
-          Arrays.sort(data1);
-          t2 = System.currentTimeMillis();
-          btime+= t2 - t1;
-          if(!Arrays.equals(data1,data2)){
-            System.out.println("FAIL TO SORT!");
-            System.exit(0);
-          }
-        }
-        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
-      }
-      System.out.println();
-    }
-
-  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
-  //int[]MAX_LIST = {1000000000,10};
-  for(int MAX : MAX_LIST){
-    for(int size = 31250; size < 2000001; size*=4){
-      long qtime=0;
-      long btime=0;
-      //average of 5 sorts.
-      for(int trial = 0 ; trial <=5; trial++){
-        int []data1 = new int[size];
-        int []data2 = new int[size];
-        for(int i = 0; i < data1.length; i++){
-          data1[i] = (int)(Math.random()*MAX);
-          data2[i] = data1[i];
-        }
-        long t1,t2;
-        t1 = System.currentTimeMillis();
-        quickSort(data2);
-        t2 = System.currentTimeMillis();
-        qtime += t2 - t1;
-        t1 = System.currentTimeMillis();
-        Arrays.sort(data1);
-        t2 = System.currentTimeMillis();
-        btime+= t2 - t1;
-        if(!Arrays.equals(data1,data2)){
-          System.out.println("FAIL TO SORT!");
-          System.exit(0);
-        }
-      }
-      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
-    }
-    System.out.println();
-  }
-}
 }
